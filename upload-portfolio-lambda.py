e367bb03b750ac8286b5389c7c955e7ffe6a3bed
Rakesh Patel <rakesh7305@gmail.com>
2020-02-02 17:12:55 -0500

    adding upload-portfolio-lambda.py

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,62 @@
+import json
+import boto3
+from botocore.client import Config
+import io
+#from io import StringIO
+import zipfile
+import mimetypes
+
+def lambda_handler(event, context):
+    # TODO implement
+    s3 = boto3.resource('s3')
+
+    portfolio_bucket = s3.Bucket('portfolio.rakeshpatel.info')
+
+    build_bucket = s3.Bucket('portfoliobuild.rakeshpatel.info')
+    portfolio_zip = io.BytesIO()
+
+    build_bucket.download_fileobj('mybuild/portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj,nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
+#
+# import boto3
+# from botocore.client import Config
+# import io
+# #from io import StringIO
+# import zipfile
+# import mimetypes
+#
+# #s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+# s3 = boto3.resource('s3')
+#
+# portfolio_bucket = s3.Bucket('portfolio.rakeshpatel.info')
+#
+# build_bucket = s3.Bucket('portfoliobuild.rakeshpatel.info')
+#
+# #for obj in portfolio_bucket.objects.all():
+# #    print (obj.key)
+#
+# #portfolio_zip = StringIO.StrinIO()
+# portfolio_zip = io.BytesIO()
+#
+# #build_bucket.download_file('portfoliobuild.zip', '/tmp/portfolio.zip')
+#
+# #zipObj = build_bucket('mybuild/portfoliobuild.zip')
+#
+# build_bucket.download_fileobj('mybuild/portfoliobuild.zip', portfolio_zip)
+#
+# with zipfile.ZipFile(portfolio_zip) as myzip:
+#     for nm in myzip.namelist():
+#         obj = myzip.open(nm)
+#         portfolio_bucket.upload_fileobj(obj,nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+#         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
